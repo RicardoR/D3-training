931ce72dc570eca4fe0479911623204a6837e038
Ricardo <rirova81@gmail.com>
2020-05-21 19:46:48 +0200

    Add second project

---
+++ Udemy course/5.10.0/js/main.js
@@ -0,0 +1,132 @@
+/*
+ *    main.js
+ *    Mastering Data Visualization with D3.js
+ *    Project 2 - Gapminder Clone
+ */
+
+// Write scales for each axis (GDP-per-capita on the x-axis, life expectancy on the y-axis)
+var margin = { left: 80, right: 20, top: 50, bottom: 100 };
+var width = 800 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
+
+var dataIndex = 0;
+
+var svg = d3
+  .select("#chart-area")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", `translate(${margin.left}+${margin.top})`);
+
+var x = d3.scaleLog().base(10).range([0, width]).domain([142, 150000]);
+var y = d3.scaleLinear().range([height, 0]).domain([0, 90]);
+
+var area = d3
+  .scaleLinear()
+  .range([25 * Math.PI, 1500 * Math.PI])
+  .domain([2000, 1400000000]);
+
+var continentColor = d3.scaleOrdinal(d3.schemePastel1);
+
+var xAxisCall = d3
+  .axisBottom(x)
+  .tickValues([400, 4000, 40000])
+  .tickFormat(d3.format("$"));
+
+var yAxisCall = d3.axisLeft(y);
+
+svg
+  .append("g")
+  .attr("class", "bottom-axis")
+  .attr("transform", `translate(0, ${height})`)
+  .call(xAxisCall);
+
+svg.append("g").attr("class", "left-axis").call(yAxisCall);
+
+svg
+  .append("text")
+  .attr("class", "y-axis-label")
+  .attr("x", -height / 2)
+  .attr("y", -margin.left + 40)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .attr("transform", "rotate(-90)")
+  .text("Life Expectancy (Years)");
+
+svg
+  .append("text")
+  .attr("class", "x-axis-label")
+  .attr("x", width / 2)
+  .attr("y", height + margin.bottom - 50)
+  .attr("font-size", "20px")
+  .attr("text-anchor", "middle")
+  .text("GDP Per Capita ($)");
+
+var yearsLabel = svg
+  .append("text")
+  .attr("class", "label-x-axis")
+  .attr("x", width - 40)
+  .attr("y", height - 10)
+  .attr("font-size", "40px")
+  .attr("opacity", "0.4")
+  .attr("text-anchor", "middle")
+  .text("1800");
+
+// Loading data:
+d3.json("data/data.json").then(function (data) {
+  console.log(data);
+  const dataCleaned = cleanData(data);
+  console.log(dataCleaned);
+  const arrayLength = dataCleaned.length - 1;
+  d3.interval(function () {
+    dataIndex = dataIndex < 214 ? dataIndex + 1 : 0;
+    update(dataCleaned[dataIndex]);
+  }, 100);
+
+  update(dataCleaned[dataIndex]);
+});
+
+function cleanData(data) {
+  return data.map((data) => {
+    return data["countries"]
+      .filter((country) => {
+        var countryData = country.income && country.life_exp;
+        return countryData;
+      })
+      .map(function (country) {
+        country.income = +country.income;
+        country.life_exp = +country.life_exp;
+        return country;
+      });
+  });
+}
+
+function update(data) {
+  var t = d3.transition().duration(100);
+
+  // 1.- Join new data with old elements
+  var circles = svg.selectAll("circle").data(data, function (d) {
+    return d.country;
+  });
+
+  // with this circles goes more quick why?
+  // var circles = svg.selectAll("circle").data(data);
+
+  // 2.- Exit old elements not represent in new data
+  circles.exit().attr("class", "exit").remove();
+
+  // 3.-  Enter new Elements present in new data
+  circles
+    .enter()
+    .append("circle")
+    .attr("class", "enter")
+    .attr("fill", (d) => continentColor(d.continent))
+    .merge(circles)
+    .transition(t)
+    .attr("cx", (d) => x(d.income))
+    .attr("cy", (d) => y(d.life_exp))
+    .attr("r", (d) => Math.sqrt(area(d.population) / Math.PI));
+
+  yearsLabel.text(dataIndex + 1800);
+}
