24755ab3cdd2b09d8ef7aa9bb5a28ae360415e60
Ricardo <rirova81@gmail.com>
2020-05-24 12:09:21 +0200

    Refactor and complete time slide in

--- Udemy_course/6.10.0/js/main.js
+++ Udemy_course/6.10.0/js/main.js
@@ -8,9 +8,8 @@ var margin = { left: 80, right: 100, top: 50, bottom: 100 },
   height = 500 - margin.top - margin.bottom,
   width = 800 - margin.left - margin.right;
 
-var data;
-
-// TODO: can be removed those 2 vars ? check it
+var originalData;
+var filteredData;
 var coinSelected = "bitcoin";
 var cryptocurrency = "price_usd";
 
@@ -24,13 +23,17 @@ var g = svg
   .append("g")
   .attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
 
-var t = d3.transition().duration(100);
+var t = function () {
+  return d3.transition().duration(1000);
+};
 
 // Time parser for x-scale
 var parseTime = d3.timeParse("%d/%m/%Y");
+var formatTime = d3.timeFormat("%d/%m/%Y");
+
 // For tooltip
 var bisectDate = d3.bisector(function (d) {
-  return d.year;
+  return d.date;
 }).left;
 
 // Scales
@@ -42,7 +45,7 @@ var formatSi = d3.format(".2s");
 
 // Axis generators
 var xAxisCall = d3.axisBottom().ticks(4);
-var yAxisCall = d3.axisLeft().tickFormat(formatAbbreviation);
+var yAxisCall = d3.axisLeft();
 
 // Axis groups
 var xAxis = g
@@ -77,68 +80,63 @@ var path = g
   .attr("stroke", "grey")
   .attr("stroke-with", "3px");
 
-// Line path generator
-var line = d3
-  .line()
-  .x(function (d) {
-    return x(d.year);
-  })
-  .y(function (d) {
-    return y(d.value);
-  });
-
 // Listeners
 $("#coin-select").on("change", () => updateData());
 $("#var-select").on("change", () => updateData());
-
+$("#date-slider").slider({
+  max: parseTime("31/10/2017").getTime(),
+  min: parseTime("12/5/2013").getTime(),
+  step: 86400000, // One day
+  values: [parseTime("12/5/2013").getTime(), parseTime("31/10/2017").getTime()],
+  slide: function (event, ui) {
+    $("#dateLabel1").text(formatTime(new Date(ui.values[0])));
+    $("#dateLabel2").text(formatTime(new Date(ui.values[1])));
+    updateData();
+  },
+});
 // Load data and init graph
-d3.json("data/coins.json").then(function (dataRetreived) {
-  data = dataRetreived;
+d3.json("data/coins.json").then(function (data) {
+  filteredData = {};
+  for (var coin in data) {
+    if (!data.hasOwnProperty(coin)) {
+      continue;
+    }
+
+    filteredData[coin] = data[coin].filter(function (d) {
+      return !(d["price_usd"] == null);
+    });
+    filteredData[coin].forEach(function (d) {
+      d["price_usd"] = +d["price_usd"];
+      d["24h_vol"] = +d["24h_vol"];
+      d["market_cap"] = +d["market_cap"];
+      d["date"] = parseTime(d["date"]);
+    });
+  }
   updateData();
 });
 
 function updateData() {
   coinSelected = $("#coin-select").val();
   cryptocurrency = $("#var-select").val();
+  sliderValues = $("#date-slider").slider("values");
 
-  // Filter data
-  filteredData = data[coinSelected]
-    .filter(function (d) {
-      return d[cryptocurrency];
-    })
-    .map((d) => {
-      d.year = parseTime(d.date);
-      d.value = +d[cryptocurrency];
-      return d;
-    });
-
-  console.log("original data", data);
-  console.log("filtered data", filteredData);
+  var dataTimeFiltered = filteredData[coinSelected].filter(function (d) {
+    return d.date >= sliderValues[0] && d.date <= sliderValues[1];
+  });
 
   // Set scale domains
-  x.domain(
-    d3.extent(filteredData, function (d) {
-      return d.year;
-    })
-  );
+  x.domain(d3.extent(dataTimeFiltered, (d) => d.date));
   y.domain([
-    d3.min(filteredData, function (d) {
-      return d.value;
-    }) / 1.005,
-    d3.max(filteredData, function (d) {
-      return d.value;
-    }) * 1.005,
+    d3.min(dataTimeFiltered, (d) => d[cryptocurrency]) / 1.005,
+    d3.max(dataTimeFiltered, (d) => d[cryptocurrency]) * 1.005,
   ]);
 
   var label = getLabelFomCurrencySelected();
   yAxisLabel.text(label);
 
   // Generate axes once scales have been set
-  xAxis.transition(t).call(xAxisCall.scale(x));
-  yAxis.transition(t).call(yAxisCall.scale(y).tickFormat(formatAbbreviation));
-
-  // Add line to path generated above
-  path.transition(t).attr("d", line(filteredData));
+  xAxis.call(xAxisCall.scale(x)).transition(t);
+  yAxis.call(yAxisCall.scale(y).tickFormat(formatAbbreviation)).transition(t);
 
   /******************************** Tooltip Code ********************************/
 
@@ -174,15 +172,29 @@ function updateData() {
 
   function mousemove() {
     var x0 = x.invert(d3.mouse(this)[0]),
-      i = bisectDate(filteredData, x0, 1),
-      d0 = filteredData[i - 1],
-      d1 = filteredData[i],
-      d = x0 - d0.year > d1.year - x0 ? d1 : d0;
-    focus.attr("transform", "translate(" + x(d.year) + "," + y(d.value) + ")");
-    focus.select("text").text(() => d3.format("$,")(d.value.toFixed(2)));
-    focus.select(".x-hover-line").attr("y2", height - y(d.value));
-    focus.select(".y-hover-line").attr("x2", -x(d.year));
+      i = bisectDate(dataTimeFiltered, x0, 1),
+      d0 = dataTimeFiltered[i - 1],
+      d1 = dataTimeFiltered[i],
+      d = x0 - d0.date > d1.date - x0 ? d1 : d0;
+    focus.attr(
+      "transform",
+      "translate(" + x(d.date) + "," + y(d[cryptocurrency]) + ")"
+    );
+    focus
+      .select("text")
+      .text(() => d3.format("$,")(d[cryptocurrency].toFixed(2)));
+    focus.select(".x-hover-line").attr("y2", height - y(d[cryptocurrency]));
+    focus.select(".y-hover-line").attr("x2", -x(d.date));
   }
+
+  // Line path generator
+  var line = d3
+    .line()
+    .x((d) => x(d.date))
+    .y((d) => y(d[cryptocurrency]));
+
+  // Add line to path generated above
+  path.transition(t).attr("d", line(dataTimeFiltered));
 }
 
 // Helpers:
